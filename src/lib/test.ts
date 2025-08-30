d37e048ca23793ff23d5f2ff61a481cac510949d
takeseem <takeseem@gmail.com>
2025-08-30 10:29:58 +0800

    Bug: [@typescript-eslint/no-unused-vars] vscode report unused, but run eslint not report #11540

---
+++ src/lib/test.ts
@@ -0,0 +1,24 @@
+export class SafeMessager {
+	private winHandlers: Map<Window, string> = new Map();
+
+	/** command eslint correct report: `abc, handler, win` is unused */
+	havFun1(abc?: string) {
+		this.winHandlers.forEach((handler, win) => {
+			if (typeof handler !== 'function') {
+				console.log('check: win is closed, call unbind');
+			}
+		});
+	}
+
+	/**
+	 * eslint can not report `handler` is unused
+	 * maybe the last arg `win` is used, so skip the check before of `win` args
+	 */
+	havFun2(abc?: string) {
+		this.winHandlers.forEach((handler, win) => {
+			if (win.closed) {
+				console.log('check: win is closed, call unbind');
+			}
+		});
+	}
+}
